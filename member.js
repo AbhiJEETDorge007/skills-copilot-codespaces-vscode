4a0820f75bef47688045d3190973dc6d4699cc24
AbhiJEETDorge007 <abhijit.dorge@ilink-systems.com>
2025-01-14 17:20:23 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+  // Skills
+  var skills = [
+    {
+      title: "HTML",
+      percentage: "80%",
+    },
+    {
+      title: "CSS",
+      percentage: "70%",
+    },
+    {
+      title: "JavaScript",
+      percentage: "60%",
+    },
+    {
+      title: "Python",
+      percentage: "40%",
+    },
+  ];
+  return skills;
+}
